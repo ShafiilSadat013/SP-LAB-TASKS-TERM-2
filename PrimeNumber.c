ab9c0f5e885e6abcdeb343e8b716788eb582a090
Shafiil Ahmed Sadat <sadatshafiil@gmail.com>
2024-09-28 11:15:41 +0600

    Create PrimeNumber.c

---
+++ PrimeNumber.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x<2)
+    {
+        printf("%d is not prime",x);
+    return 0;
+    }
+    for(int i=2;i<=x/2;i++)
+    {
+        if(x%i==0)
+        {
+            printf("%d is not prime",x);
+            return 0;
+        }
+    }
+
+    printf("%d is prime",x);
+
+    return 0;
+
+}
