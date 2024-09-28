866cce0109c28a26fe605012220bb8671572820e
Shafiil Ahmed Sadat <sadatshafiil@gmail.com>
2024-09-28 10:50:53 +0600

    Create SumOfDigits.c

---
+++ SumOfDigits.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+    int x,sum=0;
+    scanf("%d",&x);
+    while(x!=0)
+    {
+        sum=sum + (x%10);
+        x=x/10;
+    }
+    printf("sum is %d",sum);
+    return 0;
+
+}
