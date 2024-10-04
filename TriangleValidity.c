de920c18b929b72be42f1e3b83c995ebde7860f1
Shafiil Ahmed Sadat <sadatshafiil@gmail.com>
2024-10-04 13:59:18 +0600

    Update TriangleValidity.c

--- TriangleValidity.c
+++ TriangleValidity.c
@@ -6,6 +6,7 @@ int main()
     scanf("%f%f%f",&x,&y,&z);
     if(x<0&&y<0&&z<0) printf("invalid input\n");
     else if(((x+y)>z)&&((x+z)>y)&&((y+z)>x)) printf("The triangle is valid\n");
+    else printf("the triangle is invalid");
     return 0;
 
 
